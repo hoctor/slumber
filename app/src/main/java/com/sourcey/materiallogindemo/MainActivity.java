bce5aac1049f5e82c407a15afd7b67323bce5646
junotomo <jun.atomo@gmail.com>
2017-05-21 19:16:21 -0300

    make arquivo.csv
    sdsadasda

--- app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
+++ app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.sourcey.materiallogindemo;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.text.DecimalFormat;
+import android.icu.util.Calendar;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.hardware.Sensor;
@@ -18,11 +23,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends Activity implements SensorEventListener,View.OnClickListener {
 
-    private TextView xText, yText, zText;
+    private TextView xText;
     private Sensor sensor;
     private SensorManager sManager;
     private Button startB,stopB;
@@ -30,8 +40,7 @@ public class MainActivity extends Activity implements SensorEventListener,View.O
     DecimalFormat dx = new DecimalFormat("#.##");
     DecimalFormat dy = new DecimalFormat("#.##");
     DecimalFormat dz = new DecimalFormat("#.##");
-
-
+    public String FILENAME = "motion.csv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,26 +48,23 @@ public class MainActivity extends Activity implements SensorEventListener,View.O
         setContentView(R.layout.activity_main);
 
         //Create sensor manager
-        sManager =(SensorManager)getSystemService(SENSOR_SERVICE);
+        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         // Accelerometer sensor
         sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         //Register sensor Listener
-        sManager.registerListener(this, sensor,SensorManager.SENSOR_DELAY_NORMAL);
+        sManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         // assign Textview
-        xText = (TextView)findViewById(R.id.xText);
-        yText = (TextView)findViewById(R.id.yText);
-        zText = (TextView)findViewById(R.id.zText);
+        xText = (TextView) findViewById(R.id.xText);
+
         Button startButton = (Button) findViewById(R.id.startB);
         Button stopButton = (Button) findViewById(R.id.stopB);
         startButton.setOnClickListener(this);
         stopButton.setOnClickListener(this);
         mInitialized = false;
     }
-
     public void onResume() {
         super.onResume();
                 sManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-
     }
     protected void onPause() {
         super.onPause();
@@ -66,17 +72,21 @@ public class MainActivity extends Activity implements SensorEventListener,View.O
     }
 
 
-        public void onAccuracyChanged(Sensor sensor, int acc) { }
+        public void onAccuracyChanged(Sensor sensor, int acc) {}
+
 
 
         public void onSensorChanged(SensorEvent event) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
+            xText.setText(" X  :"+ Float.toString(event.values[0]) +"\n"+
+                    " Y  :"+ Float.toString(event.values[1]) +"\n"+
+                    " Z :"+ Float.toString(event.values[2]));
 
-            xText.setText("X : " + dx.format(x));
-            yText.setText("Y : " + dy.format(y));
-            zText.setText("Z : " + dz.format(z));
+            try {
+                writeToCsv(Float.toString(event.values[0]),Float.toString(event.values[1]),Float.toString(event.values[2]));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
 
         public void onClick (View v){
@@ -89,4 +99,23 @@ public class MainActivity extends Activity implements SensorEventListener,View.O
                     break;
             }
     }
-}
+
+    public void writeToCsv(String x,String y,String z) throws IOException {
+       Calendar c = Calendar.getInstance();
+        File file = new File(Environment.getDataDirectory () + "/TollCulator");
+        boolean success = true;
+        if (!file.exists()) {
+            success = file.mkdir();
+        }
+        if (success) {
+            String csv = "/storage/emulated/Android/data/com.sourcey.materialloginexample/files/motion.csv";
+            FileWriter file_writer = new FileWriter(csv, true);
+            String s = c.get(Calendar.HOUR) + "," + c.get(Calendar.MINUTE) + "," + c.get(Calendar.SECOND) + "," + c.get(Calendar.MILLISECOND) + "," + x + "," + y + "," + z + "\n";
+            file_writer.append(s);
+            file_writer.close();
+        }
+    }
+
+
+
+    }
